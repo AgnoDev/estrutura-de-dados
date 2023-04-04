fcccc5e17190d8e896204141c336c92a1689aac5
AgnoDev <agnodev87@gmail.com>
2023-04-04 17:14:37 -0300

    implementa método 'add' na classe ListaEncadeada

--- src/main/java/com/projeto/listaencadeada/ListaEncadeada.java
+++ src/main/java/com/projeto/listaencadeada/ListaEncadeada.java
@@ -8,6 +8,21 @@ public class ListaEncadeada<T> {
         this.refEntrada = null;
     }
 
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+        if(this.isEmpty()){
+            refEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = refEntrada;
+        for(int i = 0, i < this.size()-1, i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
     public int size() {
         int tamanhoLista = 0;
         No<T> referenciaAux = refEntrada;

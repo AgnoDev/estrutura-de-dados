93eafb2f5af87c6a666d6a3fb947737bc48ea616
AgnoDev <agnodev87@gmail.com>
2023-04-04 11:03:48 -0300

    demonstra atribuicoes e referencias

---
+++ src/main/java/com/projeto/atribuicoesereferencias/Main.java
@@ -0,0 +1,23 @@
+package com.projeto.atribuicoesereferencias;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int intA = 1;
+        int intB = intA;
+        System.out.println("intA = " + intA + " intB = " + intB);
+        intA = 2;
+        System.out.println("intA = " + intA + " intB = " + intB);
+
+        MeuObj objA = new MeuObj(1);
+        MeuObj objB = objA;
+        System.out.println("objA = " + objA + " objB = " + objB);
+
+        objA.setNum(2);
+        System.out.println("objA = " + objA + " objB = " + objB);
+
+    }
+
+}

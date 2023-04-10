7af27b9265c880488c19e26c60652f31a01475f7
AgnoDev <agnodev87@gmail.com>
2023-04-10 14:28:41 -0300

    implementa metodos 'get' e 'getNo'

--- src/main/java/com/projeto/listascirculares/ListaCircular.java
+++ src/main/java/com/projeto/listascirculares/ListaCircular.java
@@ -1,16 +1,33 @@
 package com.projeto.listascirculares;
 
-public class ListaCircular <T>{
+public class ListaCircular<T> {
 
     private NoListaCirculares<T> cauda;
     private NoListaCirculares<T> cabeca;
     private int tamanhoLista;
 
-    public boolean isEmpty(){
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private NoListaCirculares<T> getNo(int index) {
+        if (this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia!");
+        if(index == 0){
+            return this.cauda;
+        }
+        NoListaCirculares<T> noAuxiliar = this.cauda;
+        for(int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noAuxiliar;
+    }
+
+    public boolean isEmpty() {
         return this.tamanhoLista == 0 ? true : false;
     }
 
-    public int size(){
+    public int size() {
         return this.tamanhoLista;
     }
 }

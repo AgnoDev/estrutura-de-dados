55b528a332a193b7a6b80c46bd29f37267d8a97c
AgnoDev <agnodev87@gmail.com>
2023-04-10 10:56:42 -0300

    implementa metodo 'size'

---
+++ src/main/java/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -0,0 +1,19 @@
+package com.projeto.listaduplamenteencadeada;
+
+public class ListaDuplamenteEncadeada<T>{
+
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+
+    private int tamanhoLista;
+
+    public ListaDuplamenteEncadeada(){
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+        this.tamanhoLista = 0;
+    }
+
+    public int size(){
+        return tamanhoLista;
+    }
+}

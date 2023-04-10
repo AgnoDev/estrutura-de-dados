60738e447e1920ffe35ad6ba066f30e4532ea0c0
AgnoDev <agnodev87@gmail.com>
2023-04-10 11:54:52 -0300

    implementa metodos 'remove'

--- src/main/java/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ src/main/java/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -54,6 +54,25 @@ public class ListaDuplamenteEncadeada<T> {
 
     }
 
+    public void remove(int index) {
+
+        if (index == 0) {
+            primeiroNo = primeiroNo.getNoProximo();
+            if (primeiroNo != null) {
+                primeiroNo.setNoPrevio(null);
+            }
+        } else {
+            NoDuplo<T> noAuxiliar = getNo(index);
+            noAuxiliar.getNoPrevio().setNoProximo(noAuxiliar.getNoProximo());
+            if (noAuxiliar != ultimoNo) {
+                noAuxiliar.getNoProximo().setNoPrevio((noAuxiliar.getNoPrevio()));
+            } else {
+                ultimoNo = noAuxiliar;
+            }
+        }
+        this.tamanhoLista--;
+    }
+
     private NoDuplo<T> getNo(int index) {
         NoDuplo<T> noAuxiliar = primeiroNo;
         for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
